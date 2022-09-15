084161ca78448572d20afd31be82bfafaea6ce79
U-BSD\403313 <403313@SHS-DGDRCS2.beaverton.k12.or.us>
2022-09-15 12:21:14 -0700

    Palindrome V2

--- pdrome.cpp
+++ pdrome.cpp
@@ -10,25 +10,42 @@ using namespace std;
 int main() {
   char input[81];
   char check[81];
+  char compare[81];
   cout << "Put in a Word, Sentence, or phrase." << endl;
   cout << "This program will tell you if it is a palindrome" << endl;
   cout << "Don't input something longer than 80 characters. If you do your line will be shortened to 80 characters" << endl;
   cin.get(input, 81);
   cin.get();
-  cout << input << endl;
   int count = 0;
   for (int i = 0; i < 80; i++) {
-    cout << "Ascii value of " << input[i] << " is equal to: " << int(input[i]) << endl;
-    if (int(input[i]) > 64 && int(input[i]) < 91) {
-      int(input[i]) += 32;
-      check[count] = input[i];
-      count++;
+    if (input[i] != '\0') {
+      int ascii = int(input[i]);
+      if (ascii > 64 && ascii < 91) {
+	ascii += 32;
+	//cout << "Ascii value is " << ascii << endl;
+	check[count] = char(ascii);
+	count++;
+      }
+      else if (ascii > 96 && ascii < 123) {
+	//cout << "Ascii value is " << ascii < endl;
+	check[count] = input[i];
+	count++;
+      }
     }
-    else if (int(input[i]) > 96 && int(input[i]) < 123) {
-      check[count] = input[i];
-      count++;
+    else {
+      i = 80;
     }
   }
-  // Add ending char to check
   cout << check << endl;
+  count << count << endl;
+  for (int i = 0; i < count; i++) {
+    if (check[i] != '\0') {
+      compare[count] = check[i];
+      count--;
+    }
+    else {
+      i = 80;
+    }
+  }
+  cout << compare << endl;
 }
