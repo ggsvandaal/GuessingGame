31df1e6c9bb4f0b04fe756e96fb92de8497b552e
U-BSD\403313 <403313@SHS-DGDRCS2.beaverton.k12.or.us>
2023-02-17 14:32:50 -0800

    first commit

---
+++ hash/student.h
@@ -0,0 +1,22 @@
+#ifndef STUDENT
+#define STUDENT
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Student {
+public:
+  Student(char* Name, char* Lastname, int Id, float GPA);
+  ~Student();
+  char* getName();
+  char* getLastname();
+  int getId();
+  float getGPA();
+private:
+  char name[80];
+  char lastname[80];
+  int id;
+  float gpa;
+};
+#endif
