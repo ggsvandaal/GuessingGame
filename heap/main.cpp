df42c74609d0fa192772a0c4dd059096b5a470aa
U-LAPTOP-TD20MLKH\svand <svandaal@gmail.com>
2023-03-01 18:08:46 -0800

    Third Commit

--- heap/main.cpp
+++ heap/main.cpp
@@ -1,60 +1,97 @@
+/*
+  Heap
+  Author: Samual van Daal
+  Date: 3/1/2023
+*/
+
 #include <iostream>
 #include <fstream>
 #include <cstring>
 
 using namespace std;
 
-void add(int* a, int b);
-void print(int* a);
-void remove(int* a);
-void rmall(int* a);
+// Function Prototypes
+void add(int* a, int b); // Add
+void print(int* a, int b, int c); // Print
+void remove(int* a); // Remove Root
+void rmall(int* a); // Remove All
 
+// Main
 int main() {
-  int heap[101];
+  
+  int heap[101]; // Heap Initialize
+
+  // Setup //
+  
   char ask[20];
   int input;
   for (int i = 0; i < 102; i++) {
     heap[i] = 0;
   }
+  heap[0] = -1;
   cout << "Type INPUT, REMOVE, REMOVE ALL, PRINT" << endl;
   bool play = true;
+
+  // Main Loop //
+  
   while (play == true) {
     cout << ": " ;
     cin.get(ask, 20);
     cin.ignore(10, '\n');
+    
+    // Typed INPUT
     if (strcmp(ask, "INPUT") == 0) {
       cout << "Input your number" << endl;
       cin >> input;
       cin.ignore(10, '\n');
-      add(heap, input);
+      add(heap, input); // Add Function
     }
+    
+    // Typed PRINT
     if (strcmp(ask, "PRINT") == 0) {
-      print(heap);
+      int temp[101];
+      for (int i = 0; i < 102; i++) {
+	temp[i] = heap[i];
+      }
+      print(temp, 0, 1); // Print Function
     }
+    
+    // Typed QUIT
     if (strcmp(ask, "QUIT") == 0) {
-      play = false;
+      play = false; // End Loop
     }
+    
+    // Typed REMOVE
     if (strcmp(ask, "REMOVE") == 0) {
-      remove(heap);
+      remove(heap); // Remove Root Function
     }
+    
+    // Typed REMOVE ALL
     if (strcmp(ask, "REMOVE ALL") == 0) {
-      rmall(heap);
+      rmall(heap); // Remove all function
     }
+    
   }
 }
+
+// Add Function //
 
 void add(int* heap, int input) {
   int place;
   int temp;
-  for (int i = 1; i < 102; i++) {
-    if (heap[i] == 0) {
-      heap[i] = input;
+
+  // Add to Heap
+  for (int i = 1; i < 102; i++) { // Loop through heap array
+    if (heap[i] == 0) { // Finds closest available spot in the heap
+      heap[i] = input; // Add input to heap
       place = i;
       break;
     }
   }
-  for (int i = place; i > 1; i--) {
-    if (heap[i] > heap[i/2]) {
+
+  // Sort
+  for (int i = place; i > 1; i--) { // Loop from index of input -> index of root
+    if (heap[i] > heap[i/2]) { // If child > parent, Swap their places
       temp = heap[i/2];
       heap[i/2] = heap[i];
       heap[i] = temp;
@@ -62,35 +99,82 @@ void add(int* heap, int input) {
   }
 }
 
-void print(int* heap) {
+// Print Function //
+
+void print(int* heap, int depth, int current) {
+
+  // Debug Print
+  /*
   for (int i = 1; i < 102; i++) {
     cout << heap[i] << " ";
-  }
+    }
   cout << endl;
+  */
+
+  // Tree Print
+  
+  if (heap[current] == 0 && current != 0) {
+    print(heap, depth-1, current/2);
+  }
+
+  if (heap[(current*2)+1] != 0) {
+    current = (current*2)+1;
+    print(heap, depth+1, current);
+  }
+  
+  for (int i = 0; i < depth; i++) {
+    cout << '\t';
+  }
+  cout << heap[current] << endl;
+  heap[current] = 0;
+  
+  if (heap[current*2] != 0) {
+    current = current*2;
+    print(heap, depth+1, current);
+  }
+
+  if (heap[(current*2)+1] == 0 && heap[current*2] == 0) {
+    heap[current] = 0;
+    current = current/2;
+    
+    if (heap[1] != 0) {
+      print(heap, depth-1, current);
+    }
+  }
 }
+
+// Remove Root Function //
 
 void remove(int* heap) {
   int last;
   int temp;
   int current = 1;
-  for (int i = 1; i < 102; i++) {
-    if (heap[i+1] == 0) {
+
+  // Root and Last Swap
+  for (int i = 1; i < 102; i++) { // Loop through heap
+    if (heap[i+1] == 0) { // Find last filled child
       last = heap[i];
-      heap[i] = 0;
+      heap[i] = 0; // Delete last
       break;
     }
   }
-  heap[1] = last;
+  heap[1] = last; // Root = Last
+
+  // Loop until value of last is lower than both of its children
   while(heap[current*2] > heap[current] || heap[(current*2)+1] > heap[current]) {
     int left = heap[current*2];
     int right = heap[(current*2)+1];
-    if (left > right) {
+
+    // Left > Right
+    if (left > right) { // Swaps Parent with left
       temp = left;
       heap[current*2] = heap[current];
       heap[current] = temp;
       current = current*2;
     }
-    else if (right > left) {
+
+    // Right > Left
+    else if (right > left) { // Swaps Parent with Right
       temp = right;
       heap[(current*2)+1] = heap[current];
       heap[current] = temp;
@@ -99,9 +183,10 @@ void remove(int* heap) {
   }
 }
 
+// Remove All Function //
 
 void rmall(int* heap) {
-  for (int i = 0; i < 102; i++) {
-    heap[i] = 0;
+  for (int i = 0; i < 102; i++) { // Loop Through Heap
+    heap[i] = 0; // Remove Every value
   }
 }
