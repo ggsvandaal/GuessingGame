67d29b240a6e449ddd87b7208888265ccac11d6b
U-BSD\403313 <403313@SHS-DGDRCS2.beaverton.k12.or.us>
2023-03-02 12:24:09 -0800

    setup

---
+++ shunt_yard/node.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+#include "node.h"
+
+using namespace std;
+
+Node::Node(char Value) {
+  value = Value
+};
+
+char Node::getValue() {
+  return value;  
+}
+
+
+Node* Node::getRight() {
+  return right;
+}
+
+Node* Node::getLeft() {
+  return left;
+}
+
+Node* Node::getPrev() {
+  return prev;
+}
+
+void Node::setRight(Node* Right) {
+  right = Right;
+}
+
+void Node::setLeft(Node* Left) {
+  left = Left;
+}
+
+void Node::setPrev(Node* Prev) {
+  prev = Prev;
+}
+
+Node::~Node() {
+  delete student;
+}
---
+++ shunt_yard/node.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Node { // Node Class
+public:
+  Node(char Value); // Node Constructor
+  ~Node(); // Destructor
+  char getValue(); // get student*
+  void setRight(Node*); // Set the right node
+  void setLeft(Node*); // Set the left node
+  void setPrev(Node*); // Set the previous node
+  Node* getRight(); // Get the right node
+  Node* getLeft(); // Get the left node
+  Node* getPrev(); // Get the previous node
+private:
+  Node* right;
+  Node* left;
+  Node* prev;
+  char value;
+};
+
